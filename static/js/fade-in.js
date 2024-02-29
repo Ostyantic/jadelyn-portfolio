a7d5c08badc311353b6cdcf92aef6f51b16cc311
Anthony Lopez <Antl21292@gmail.com>
2024-02-29 00:19:46 -0500

    js for fade-in effect

---
+++ static/js/fade-in.js
@@ -0,0 +1,10 @@
+'use strict';
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        document.getElementById("fade-in-text").classList.add("fade-in");
+    },  1000);
+     setTimeout(function () {
+        document.getElementById("fade-in-textp").classList.add("fade-in");
+    },  1500);
+  });
